5fffa4e9adbc58cdf962c9de890ca75c9e980a74
NaraeJang <j.naaaarae@gmail.com>
2023-06-30 17:49:44 -0400

    js: create functions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Constructor function : To create multiple instances.
 function Counter(element, value) {
+  // Select elements.
   this.counter = element;
   this.value = value;
   this.decreaseBtn = element.querySelector(".decrease");
@@ -7,13 +8,7 @@ function Counter(element, value) {
   this.increaseBtn = element.querySelector(".increase");
   this.valueDOM = element.querySelector(".value");
   this.valueDOM.textContent = this.value;
-
-  console.log(this.resetBtn);
 }
-
-// Create new instances.
-const firstCounter = new Counter(getElement(".first-counter"), 100);
-const secondCounter = new Counter(getElement(".second-counter"), 200);
 
 // To get me the element.
 function getElement(selection) {
@@ -26,3 +21,23 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists.`
   ); //By adding Error, you can notice what is wrong.
 }
+
+// By using prototype, we can keep these functions in the instances and we don't make copies of that.
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+// Create new instances.
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
